2bce44ecc8d2679266442323543f84871549714f
olefirenko <dmitriy.olefirenko@gmail.com>
2020-06-28 21:57:58 +0800

    Initial version

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,21 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useNetworkStatus } from '../.';
+
+const statusStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  fontSize: '36px'
+}
 
 const App = () => {
+  const isOnline = useNetworkStatus()
   return (
-    <div>
-      <Thing />
+    <div style={statusStyle}>
+      {isOnline ? '📡 Online' : '📴 Offline'}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import { useState, useEffect } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
-}
+export const useNetworkStatus = (): boolean => {
+  const [isOnline, setIsOnline] = useState<boolean>(() =>
+    typeof navigator !== 'undefined' && typeof navigator.onLine === 'boolean'
+      ? navigator.onLine
+      : true
+  );
 
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+  useEffect(() => {
+    function setOnline() {
+      setIsOnline(true);
+    }
+
+    function setOffline() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener('online', setOnline);
+    window.addEventListener('offline', setOffline);
+
+    return () => {
+      window.removeEventListener('online', setOnline);
+      window.removeEventListener('offline', setOffline);
+    };
+  }, []);
+
+  return isOnline;
 };
